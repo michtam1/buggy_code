835deccc7ba9f9035386ee632bf9227c62f3a301
sysox <marek.sys@gmail.com>
2024-04-14 16:28:03 +0200

    buggy code update and solution

--- buggy_python_code_solution.py
+++ buggy_python_code_solution.py
@@ -47,10 +47,10 @@ def authenticate(password):
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
-    print("1. Format string vulnerability:")
-    print("2. Code injection vulnerability:")
-    print("3. Yaml deserialization vulnerability:")
-    print("4. Use of assert statements vulnerability:")
+    print("1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}")
+    print("2. Code injection vulnerability: use string=;print('Own code executed') #")
+    print("3. Yaml deserialization vulnerability: use string=file.yaml")
+    print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
     if choice == "1": 
         new_person = Person("Vickie")  
